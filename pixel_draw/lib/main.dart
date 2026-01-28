b55f5a4a67258da2eb3a5f8f3b8d23379c7cd506
gratiam <104708129+gratiam@users.noreply.github.com>
2026-01-27 18:18:58 -0700

    Refactored for drag-draw & buttons wrap

--- pixel_draw/lib/main.dart
+++ pixel_draw/lib/main.dart
@@ -59,19 +59,79 @@ class _PaintPageState extends State<PaintPage> {
         spacing: 3,
         children: [
           
-          for (var i in [0,2,5,9,14,20])
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 3,
-              children: [
-                //for (var j in [1,2])
-                for (var j in [1,2,3,4,5,6])
-                  ClickableTile(),
-              ]
-            ),
+          tileGrid(),
+          SizedBox(height: 10.0), // space out tiles and buttons
           ColorButtons(),
           
         ]
+      )
+    );
+  }
+}
+
+// small static tracker for whether there is a click or not
+class ClickTracker {
+  static bool isClicking = false;
+}
+
+class tileGrid extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) {
+        print("Mouse entered container.");
+        print(ClickTracker.isClicking);
+      },
+      onExit: (event) {
+        ClickTracker.isClicking = false;
+        print("Mouse left the container.");
+        print(ClickTracker.isClicking);
+        },
+      child: GestureDetector(
+        onTapDown: (details) {
+          ClickTracker.isClicking = true;
+          print("Started clicking.");
+          print(ClickTracker.isClicking);
+          },
+        
+        onTapUp: (details) {
+          ClickTracker.isClicking = false;
+          print("Stopped clicking.");
+          print(ClickTracker.isClicking);
+        },
+        onPanStart: (details) {
+          ClickTracker.isClicking = true;
+          print("Started panning.");
+          print(ClickTracker.isClicking);
+        },
+        onPanEnd: (details) {
+          ClickTracker.isClicking = false;
+          print("Stopped panning.");
+          print(ClickTracker.isClicking);
+        },
+
+        child: Container(
+          color: Colors.blue,
+          width: double.infinity,
+          child: Column(
+            spacing: 3,
+            children: [
+              for (var i in [0,2,5,9,14,20]) 
+                Row(
+                  
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 3,
+                  
+                  children: [
+                    //for (var j in [1,2])
+                    for (var j in [1,2,3,4,5,6])
+                      ClickableTile(),
+                  ]
+                ),
+            ]
+          )
+        )
       )
     );
   }
@@ -92,24 +152,55 @@ class _ClickableTile extends State<ClickableTile> {
 
   Color currentColor = Colors.grey;
 
+  void doClick() {
+    setState(() {
+      paintTile();
+    });
+    print(ClickTracker.isClicking);
+  }
   void paintTile() {
     currentColor = ClickableTile.brushColor;
   }
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector( // detect clicks
-      onTapDown: (details) {
-        setState(() {
-          //toggleState(); // change state (color)
-          paintTile();
-        });
+    return MouseRegion( // detect clicks
+      onEnter: (details) {
+        print("Hovered tile.");
+          print(ClickTracker.isClicking);
+        if (ClickTracker.isClicking) {
+          print("Is clicked — Setting state.");
+          doClick();
+        }
       },
-      child: Container(
-        width: 50,
-        height: 50,
-        // color: _state ? Colors.blue : Colors.grey, // blue if active; grey if not.
-        color: currentColor,
+      child: GestureDetector(
+        onTapDown: (details) {
+          doClick();
+        },
+        onPanStart: (details) { 
+          doClick();
+          ClickTracker.isClicking = true;
+          //print("Started panning");
+          //print(ClickTracker.isClicking);
+        },
+        onPanEnd: (details) {
+          ClickTracker.isClicking = false;
+          //print("Stopped Panning");
+          //print(ClickTracker.isClicking);
+        },
+        // release
+        onTapUp: (details) {
+          ClickTracker.isClicking = false;
+          //print("Untapped");
+          //print(ClickTracker.isClicking);
+        },
+        
+        child: Container(
+          width: 50,
+          height: 50,
+          // color: _state ? Colors.blue : Colors.grey, // blue if active; grey if not.
+          color: currentColor,
+        ),
       ),
     );
   }
@@ -126,7 +217,7 @@ class _ColorButtons extends State<ColorButtons> {
   int selectedIdx = 2;
   
   // List of valid colors
-  var _colors = [
+  final _colors = [
     [Colors.black, "Black"],
     [Colors.white, "White"],
     [const Color.fromRGBO(128, 16, 8, 1), "Dark Red"],
@@ -137,10 +228,10 @@ class _ColorButtons extends State<ColorButtons> {
     [Colors.brown, "Brown"],
     [Colors.yellow, "Yellow"]
   ];
-  Color getColor(int idx) {
+  Color _getColor(int idx) {
     return _colors[idx][0] as Color;
   }
-  String getColorName(int idx) {
+  String _getColorName(int idx) {
     return _colors[idx][1] as String;
   }
 
@@ -154,9 +245,10 @@ class _ColorButtons extends State<ColorButtons> {
   // structure
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-       
+    return Wrap(
+      //mainAxisAlignment: MainAxisAlignment.center,
+      spacing: 5,
+      runSpacing: 10,
       // for each element
       children: List.generate(_colors.length, (index) {
         // if selected
@@ -165,7 +257,7 @@ class _ColorButtons extends State<ColorButtons> {
             onTapDown: (details) {
               // change brush color to clicked button color
               setState(() {
-                ClickableTile.brushColor = getColor(index);
+                ClickableTile.brushColor = _getColor(index);
                 selectedIdx = index;
               });
             },
@@ -176,11 +268,11 @@ class _ColorButtons extends State<ColorButtons> {
               // has border if selected
               decoration: BoxDecoration(
                 border: index == selectedIdx ? Border.all(color:Colors.blueGrey, width:3):Border.all(color:Colors.white,width:1),
-                color: getColor(index),
+                color: _getColor(index),
               ),
               child: Center(
                 child: Text(
-                  getColorName(index), // text to display
+                  _getColorName(index), // text to display
                   style: TextStyle(
                       color:Colors.yellowAccent,
                       fontWeight: FontWeight.bold,
